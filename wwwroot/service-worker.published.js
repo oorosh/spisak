f7ae8311bf6e3fd0fc0cb1e2b57103a6dd606ed7
Uros <b.urosh@gmail.com>
2022-08-07 22:54:45 +0200

    fixed service worker

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -44,9 +44,9 @@ async function onFetch(event) {
         // For all navigation requests, try to serve index.html from cache
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
 //#if(IndividualLocalAuth && Hosted)
-        const shouldServeIndexHtml = event.request.mode === 'navigate'
-            && !event.request.url.includes('/connect/')
-            && !event.request.url.includes('/Identity/');
+        //const shouldServeIndexHtml = event.request.mode === 'navigate'
+            //&& !event.request.url.includes('/connect/')
+            //&& !event.request.url.includes('/Identity/');
 //#else
         const shouldServeIndexHtml = event.request.mode === 'navigate';
 //#endif
